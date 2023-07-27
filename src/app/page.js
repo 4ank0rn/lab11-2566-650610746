060d30fda3ce82951b21d9ff8c192e1229a1d1cb
Au <auau0197@gmail.com>
2023-07-28 00:28:33 +0700

    maybefinishnum2

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function RegisFormPage() {
   const [plan, setPlan] = useState("");
   const [planError, setPlanError] = useState(false);
   const [gender, setGender] = useState(null);
-  const [genderError, setGenderError] = useState("");
+  const [genderError, setGenderError] = useState(false);
   const [buyBottle, setBuyBottle] = useState(false);
   const [buyShoes, setBuyShoes] = useState(false);
   const [buyCap, setBuyCap] = useState(false);
